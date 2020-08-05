79602b7bb0f7d0045a68108456f42a3d537a778b
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-05 17:28:46 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,19 +1,26 @@
 require 'pry'
 
   # This is the array we will be passing into the remove_strawberry method
-  # contacts = {
-  #   "Jon Snow" => {
-  #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
-  #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
-  #   },
-  #   "Freddy Mercury" => {
-  #     name: "Freddy",
-  #     email: "freddy@mercury.com",
-  #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
-  #   }
-  # }
+  contacts = {
+    "Jon Snow" => {
+      name: "Jon",
+      email: "jon_snow@thewall.we", 
+     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+    },
+   "Freddy Mercury" => {
+      name: "Freddy",
+      email: "freddy@mercury.com",
+       favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+     }
+   }
   
 def remove_strawberry(contacts)
-
-end
+contacts.each do |person, conact_details_hash|
+if person == "Freddy Mercury"
+  conact_details_hash.each do |atribute, data|
+    if atribute == favorite_ice_cream_flavors
+      favorite_ice_cream_flavors.shift
+    end 
+  end 
+end 
+end 
